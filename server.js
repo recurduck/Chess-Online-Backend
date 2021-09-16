363bb3e02914f7f9055a5572d591f7cec80f6114
Recurduck Greco <ilaigreco@gmail.com>
2021-09-16 17:55:52 +0300

    first mobile version

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const expressSession = require('express-session')
 const cors = require('cors')
+const path = require('path')
+const expressSession = require('express-session')
+const { connectSockets } = require('./services/socket.service')
 
 const app = express()
 const http = require('http').createServer(app)
-const { connectSockets } = require('./services/socket.service')
 
 const session = expressSession({
   secret: 'coding is amazing',
@@ -12,15 +13,13 @@ const session = expressSession({
   saveUninitialized: true,
   cookie: { secure: false }
 })
+
 // Express App Config
 app.use(express.json())
 app.use(session)
 
-const boardRoutes = require('./api/board/board.routes')
-const userRoutes = require('./api/user/user.routes')
-const authRoutes = require('./api/auth/auth.routes')
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('public'));
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
@@ -28,6 +27,10 @@ if (process.env.NODE_ENV === 'production') {
   }
   app.use(cors(corsOptions))
 }
+
+const boardRoutes = require('./api/board/board.routes')
+const userRoutes = require('./api/user/user.routes')
+const authRoutes = require('./api/auth/auth.routes')
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
@@ -37,24 +40,26 @@ app.use('/api/board', boardRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Hello Chess API!')
-})
+// app.get('/', (req, res) => {
+//   res.send(__dirname)
+// })
 
-const logger = require('./services/logger.service')
 
 app.get('/api/setup-session', (req, res) => {
   req.session.connectedAt = Date.now()
   console.log('setup-session:', req.sessionID);
   res.end()
 })
+
 connectSockets(http, session)
 
 
+app.get('/**', (req, res) => { 
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
-
-app.get('/**', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); })
-
 http.listen(port, () => {
-  logger.info(`Server is running on port: ${port}`)
+  return logger.info(`Server is running on port: ${port}`)
 })
